dd09d430e1146440d8b8198568bc68f331c27b4c
PBoer <pasc.boer@gmail.com>
2015-11-20 11:01:44 +0100

    Unit test toegevoegd

---
+++ PetjeOp/PetjeOpTests/QuestionnaireUnitTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PetjeOp;
+
+namespace PetjeOpTests
+{
+    [TestClass]
+    public class QuestionnaireUnitTest
+    {
+        [TestMethod]
+        public void TestIfListExists()
+        {
+            Questionnaire questionnaire = new Questionnaire("test vrgenlijst");
+
+            Assert.IsNotNull(questionnaire.Questions);
+        }
+    }
+}
