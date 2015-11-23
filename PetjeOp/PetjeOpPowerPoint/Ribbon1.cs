7c1fff8e43ffb33f84710c938c022c174fec2890
martenaberson <martenaberson@gmail.com>
2015-11-23 12:09:26 +0100

    PowerPoint plugin toegevoegd poging 2

---
+++ PetjeOp/PetjeOpPowerPoint/Ribbon1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PowerPoint = Microsoft.Office.Interop.PowerPoint;
+using Microsoft.Office.Tools.Ribbon;
+using Office = Microsoft.Office.Core;
+
+namespace PetjeOpPowerPoint
+{
+    public partial class Ribbon1
+    {
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, RibbonControlEventArgs e)
+        {
+            PowerPoint.Slide currentSld = Globals.ThisAddIn.Application.ActivePresentation.Slides.Add(Globals.ThisAddIn.Application.ActivePresentation.Windows[1].Selection.SlideRange[1].SlideIndex + 1, Microsoft.Office.Interop.PowerPoint.PpSlideLayout.ppLayoutBlank);
+            PowerPoint.Shape textBox = currentSld.Shapes.AddTextbox(
+               Office.MsoTextOrientation.msoTextOrientationHorizontal, 250, 250, 500, 50);
+            textBox.TextFrame.TextRange.InsertAfter("Deze dia is toegevoegd met de knop");
+
+            
+
+
+
+
+            PowerPoint.Chart grafiek = currentSld.Shapes.AddChart2(-1, XlChartType.xl3DPieExploded, 50, 50, 100, 100);
+               );
+
+        }
+    }
+}
