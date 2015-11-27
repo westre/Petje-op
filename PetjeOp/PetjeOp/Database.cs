6f57f657e7aefb302bcc162a974f4e10ed6bcc55
sliscers <info@localdesign.nl>
2015-11-27 10:14:42 +0100

    Nieuwe ERD

--- PetjeOp/PetjeOp/Database.cs
+++ PetjeOp/PetjeOp/Database.cs
@@ -7,7 +7,7 @@ namespace PetjeOp
     //Question q = masterController.DB.GetQuestion(id);
     public class Database
     {
-        DatabaseDataContext db = new DatabaseDataContext();
+        DataClasses1DataContext db = new DataClasses1DataContext();
 
         public void Query()
         {
@@ -16,21 +16,21 @@ namespace PetjeOp
 
         public MultipleChoiceQuestion GetQuestion(int id)
         {
-            tblQuestion query = db.tblQuestions.SingleOrDefault(q => q.questionnr == id);
+            tblQuestion query = db.tblQuestions.SingleOrDefault(q => q.id == id);
 
             if (query!=null){                
-                MultipleChoiceQuestion question = new MultipleChoiceQuestion(query.question);
+                MultipleChoiceQuestion question = new MultipleChoiceQuestion(query.description);
                 return question;
             }
             return null;          
         }
 
-        public Questionnaire GetQuestionnaire(int id)
+        /*public Questionnaire GetQuestionnaire(int id)
         {           
-            tblQuestionnaire query = db.tblQuestionnaires.SingleOrDefault(q => q.questionnairenr == id);
+            tblQuestionnaire query = db.tblQuestionnaires.SingleOrDefault(q => q.id == id);
 
             if (query!=null){
-                Questionnaire questionnaire = new Questionnaire(query.questionnairename);
+                Questionnaire questionnaire = new Questionnaire(query.description);
                 foreach(tblLinkQuestion question in query.tblLinkQuestions.ToList())
                 {
                     questionnaire.addQuestion(new MultipleChoiceQuestion(question.tblQuestion.question));
@@ -38,43 +38,43 @@ namespace PetjeOp
                 return questionnaire;
             }
             return null;     
-        }
+        }*/
 
-        public Student GetStudent(int code)
+        public Student GetStudent(String code) // Returnt een Student als deze bestaat, anders NULL.
         {
             Student person = (from tblStudent in db.tblStudents
-                               where tblStudent.studentnr == code
+                               where tblStudent.nr == code
                                select new Student
                                {
-                                   StudentNr = tblStudent.studentnr,
+                                   StudentNr = tblStudent.nr,
                                    FirstName = tblStudent.firstname,
-                                   SurName = tblStudent.name,
-                                   GroupNr = tblStudent.groupnr
+                                   SurName = tblStudent.surname,
+                                   GroupNr = tblStudent.@class
 
                                }).FirstOrDefault();
 
             
 
             if (person!=null){
-                return person;
+                return person; // Returnt, uit database opgehaalde, Student.
             }
             return null;  
         }
-        public Teacher GetTeacher(String code)
+        public Teacher GetTeacher(String code) // Returnt een Teacher als deze bestaat, anders NULL.
         {
             Teacher person = (from tblTeacher in db.tblTeachers
-                              where tblTeacher.teachernr == code
+                              where tblTeacher.nr == code
                               select new Teacher
                               {
-                                  TeacherNr = tblTeacher.teachernr,
+                                  TeacherNr = tblTeacher.nr,
                                   FirstName = tblTeacher.firstname,
-                                  SurName = tblTeacher.name
+                                  SurName = tblTeacher.surname
 
                               }).FirstOrDefault();
 
             if (person != null)
             {
-                return person;
+                return person; // Returnt, uit database opgehaalde, Teacher.
             }
             return null;
         }
