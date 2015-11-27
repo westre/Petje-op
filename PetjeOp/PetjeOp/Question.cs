6ac19c18c4843c4c37481af0f86667ba335fce88
ThijsvanDam <thijsvandam@msn.com>
2015-11-27 10:03:43 +0100

    Vragenlijst veranderingen

--- PetjeOp/PetjeOp/Question.cs
+++ PetjeOp/PetjeOp/Question.cs
@@ -9,11 +9,6 @@ namespace PetjeOp
     public abstract class Question : IComparable<Question>
     {
         public readonly int id;
-        string jonathan
-        string je
-        string bent
-        string fucking
-        string homo
         public String Description { get; set; }
 
         // "Answer" is het goede antwoord op de vraag.           
