8364fbd58f920697e1efbbc512941b3f2314d56a
ThijsvanDam <thijsvandam@msn.com>
2015-12-01 11:23:53 +0100

    Subject Class

---
+++ PetjeOp/PetjeOp/Subject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PetjeOp
+{
+    class Subject
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Subject(int id, string name)
+        {
+            this.Id = id;
+            this.Name = name;
+        }
+    }
+}
