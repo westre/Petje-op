d03905e83d53fa8ffffc5dd16566c9b219e61385
sliscers <info@localdesign.nl>
2015-12-03 10:44:14 +0100

    Layout Merge

--- PetjeOp/PetjeOp/Student/StudentView.Designer.cs
+++ PetjeOp/PetjeOp/Student/StudentView.Designer.cs
@@ -70,7 +70,7 @@ namespace PetjeOp {
             this.pnlHeader.Dock = System.Windows.Forms.DockStyle.Top;
             this.pnlHeader.Location = new System.Drawing.Point(0, 0);
             this.pnlHeader.Name = "pnlHeader";
-            this.pnlHeader.Size = new System.Drawing.Size(1211, 59);
+            this.pnlHeader.Size = new System.Drawing.Size(1186, 59);
             this.pnlHeader.TabIndex = 2;
             // 
             // btnResults
@@ -255,7 +255,7 @@ namespace PetjeOp {
             this.viewPanel.Location = new System.Drawing.Point(2, 68);
             this.viewPanel.Margin = new System.Windows.Forms.Padding(2);
             this.viewPanel.Name = "viewPanel";
-            this.viewPanel.Size = new System.Drawing.Size(3783, 1558);
+            this.viewPanel.Size = new System.Drawing.Size(3758, 1558);
             this.viewPanel.TabIndex = 4;
             // 
             // StudentView
@@ -265,7 +265,7 @@ namespace PetjeOp {
             this.Controls.Add(this.viewPanel);
             this.Controls.Add(this.pnlHeader);
             this.Name = "StudentView";
-            this.Size = new System.Drawing.Size(1211, 486);
+            this.Dock = DockStyle.Fill;
             this.Resize += new System.EventHandler(this.logOutBtn);
             this.pnlHeader.ResumeLayout(false);
             this.pnlButton_Logout_Background.ResumeLayout(false);
