f586ed8e7172df2c9ef998d1ea496c55335f76ea
Kevin de Korte <kevdekorte@hotmail.com>
2015-12-04 17:07:08 +0100

    Comments

--- PetjeOp/PetjeOp/DatabaseListener.cs
+++ PetjeOp/PetjeOp/DatabaseListener.cs
@@ -17,24 +17,30 @@ namespace PetjeOp {
         public DatabaseListener() {
             ConnectionString = "Data Source=176.31.253.42,119;Initial Catalog=kbs2;User ID=kbs2_live;Password=12";
 
+            // Altijd zeker van zijn
             SqlDependency.Stop(ConnectionString);
             SqlDependency.Start(ConnectionString);
 
+            // Maak nieuwe connectie
             Connection = new SqlConnection(ConnectionString);
         }
 
         public void Start() {
+            // Hebben we een query
             if (TrackedQuery.Length > 0) {
+                // Maak een nieuwe command aan
                 var command = new SqlCommand(TrackedQuery, Connection) {
                     Notification = null
                 };
 
+                // Dependency aanmaken die wordt afgevuurd wanneer er ook maar iets wordt veranderd aan de resultaten van de query
                 var dependency = new SqlDependency(command);
                 dependency.OnChange += TrackedChanged;
 
                 if (Connection.State == ConnectionState.Closed)
                     Connection.Open();
 
+                // Query uitvoeren
                 SqlDataReader reader = command.ExecuteReader();
                 reader.Close();
                 Connection.Close();
@@ -47,11 +53,16 @@ namespace PetjeOp {
         void TrackedChanged(object sender, SqlNotificationEventArgs e) {
             var dependency = sender as SqlDependency;
 
+            // Het is een single-fire event, dus moeten we eerst unsubscriben
             if (dependency != null)
                 dependency.OnChange -= TrackedChanged;
 
+            // Hebben we subscribers?
             if (OnChange != null) {
+                // Dan geven we een notificatie aan al onze subscribers
                 OnChange(e);
+
+                // Dan subscriben we weer aan de dependency
                 Start();
             }
         }
