003baf48912bbf3bdb0f42c43aa9cfea60583295
lalilaloe <jonathantenhove@hotmail.com>
2015-12-07 13:03:42 +0100

    fix

---
+++ PetjeOp/PetjeOp/Login/LoginView2.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace PetjeOp.Login
+{
+    partial class LoginView2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginView2));
+            this.PnlLogin = new System.Windows.Forms.Panel();
+            this.pnlButton_Logout_Background = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Error = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.PnlLogin.SuspendLayout();
+            this.pnlButton_Logout_Background.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PnlLogin
+            // 
+            this.PnlLogin.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.PnlLogin.AutoSize = true;
+            this.PnlLogin.Controls.Add(this.pnlButton_Logout_Background);
+            this.PnlLogin.Controls.Add(this.Error);
+            this.PnlLogin.Controls.Add(this.label1);
+            this.PnlLogin.Controls.Add(this.textBox1);
+            this.PnlLogin.Location = new System.Drawing.Point(12, 12);
+            this.PnlLogin.Name = "PnlLogin";
+            this.PnlLogin.Size = new System.Drawing.Size(298, 105);
+            this.PnlLogin.TabIndex = 1;
+            // 
+            // pnlButton_Logout_Background
+            // 
+            this.pnlButton_Logout_Background.BackColor = System.Drawing.Color.Transparent;
+            this.pnlButton_Logout_Background.BackgroundImage = global::PetjeOp.Properties.Resources.Button_Background;
+            this.pnlButton_Logout_Background.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pnlButton_Logout_Background.Controls.Add(this.label2);
+            this.pnlButton_Logout_Background.Controls.Add(this.pictureBox1);
+            this.pnlButton_Logout_Background.Location = new System.Drawing.Point(28, 63);
+            this.pnlButton_Logout_Background.Margin = new System.Windows.Forms.Padding(2);
+            this.pnlButton_Logout_Background.Name = "pnlButton_Logout_Background";
+            this.pnlButton_Logout_Background.Size = new System.Drawing.Size(165, 40);
+            this.pnlButton_Logout_Background.TabIndex = 16;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Calibri", 14F);
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(41)))), ((int)(((byte)(98)))));
+            this.label2.Location = new System.Drawing.Point(61, 9);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 23);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Inloggen";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(30, 9);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(21, 25);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Error
+            // 
+            this.Error.AutoSize = true;
+            this.Error.ForeColor = System.Drawing.Color.DarkRed;
+            this.Error.Location = new System.Drawing.Point(175, 41);
+            this.Error.Name = "Error";
+            this.Error.Size = new System.Drawing.Size(120, 13);
+            this.Error.TabIndex = 5;
+            this.Error.Text = "Woops.. Er ging iets mis";
+            this.Error.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Inlog Code:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(65, 38);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // LoginView2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(322, 127);
+            this.Controls.Add(this.PnlLogin);
+            this.Name = "LoginView2";
+            this.Text = "Form1";
+            this.PnlLogin.ResumeLayout(false);
+            this.PnlLogin.PerformLayout();
+            this.pnlButton_Logout_Background.ResumeLayout(false);
+            this.pnlButton_Logout_Background.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PnlLogin;
+        public System.Windows.Forms.Panel pnlButton_Logout_Background;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label Error;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
