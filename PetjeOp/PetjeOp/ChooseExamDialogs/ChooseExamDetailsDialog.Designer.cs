591d975a53c9ef934d59d5193586abeabf6b93fa
gerritvds <gerritvds@me.com>
2015-12-09 11:30:26 +0100

    ChooseExamDetailsDialog: tekst en icon toegevoegd

--- PetjeOp/PetjeOp/ChooseExamDialogs/ChooseExamDetailsDialog.Designer.cs
+++ PetjeOp/PetjeOp/ChooseExamDialogs/ChooseExamDetailsDialog.Designer.cs
@@ -28,22 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ChooseExamDetailsDialog));
             this.btnCancel = new System.Windows.Forms.Button();
             this.btnOk = new System.Windows.Forms.Button();
             this.btnTest = new System.Windows.Forms.Button();
             this.lbExams = new System.Windows.Forms.ListView();
             this.chId = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.chLecture = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chDate = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.chStartTime = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.chEndtime = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.chDate = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SuspendLayout();
             // 
             // btnCancel
             // 
-            this.btnCancel.Location = new System.Drawing.Point(519, 269);
+            this.btnCancel.Location = new System.Drawing.Point(1038, 517);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.Size = new System.Drawing.Size(150, 44);
             this.btnCancel.TabIndex = 5;
             this.btnCancel.Text = "Cancel";
             this.btnCancel.UseVisualStyleBackColor = true;
@@ -51,9 +53,10 @@
             // 
             // btnOk
             // 
-            this.btnOk.Location = new System.Drawing.Point(438, 269);
+            this.btnOk.Location = new System.Drawing.Point(876, 517);
+            this.btnOk.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.btnOk.Name = "btnOk";
-            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.Size = new System.Drawing.Size(150, 44);
             this.btnOk.TabIndex = 4;
             this.btnOk.Text = "OK";
             this.btnOk.UseVisualStyleBackColor = true;
@@ -61,9 +64,10 @@
             // 
             // btnTest
             // 
-            this.btnTest.Location = new System.Drawing.Point(218, 269);
+            this.btnTest.Location = new System.Drawing.Point(436, 517);
+            this.btnTest.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.btnTest.Name = "btnTest";
-            this.btnTest.Size = new System.Drawing.Size(75, 23);
+            this.btnTest.Size = new System.Drawing.Size(150, 44);
             this.btnTest.TabIndex = 6;
             this.btnTest.Text = "button1";
             this.btnTest.UseVisualStyleBackColor = true;
@@ -76,9 +80,10 @@
             this.chDate,
             this.chStartTime,
             this.chEndtime});
-            this.lbExams.Location = new System.Drawing.Point(12, 12);
+            this.lbExams.Location = new System.Drawing.Point(24, 23);
+            this.lbExams.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.lbExams.Name = "lbExams";
-            this.lbExams.Size = new System.Drawing.Size(582, 251);
+            this.lbExams.Size = new System.Drawing.Size(1160, 479);
             this.lbExams.TabIndex = 7;
             this.lbExams.UseCompatibleStateImageBehavior = false;
             this.lbExams.View = System.Windows.Forms.View.Details;
@@ -93,6 +98,10 @@
             this.chLecture.Text = "Vragenlijstnaam";
             this.chLecture.Width = 300;
             // 
+            // chDate
+            // 
+            this.chDate.Text = "Datum";
+            // 
             // chStartTime
             // 
             this.chStartTime.Text = "Starttijd";
@@ -103,21 +112,19 @@
             this.chEndtime.Text = "Eindtijd";
             this.chEndtime.Width = 80;
             // 
-            // chDate
-            // 
-            this.chDate.Text = "Datum";
-            // 
             // ChooseExamDetailsDialog
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(605, 304);
+            this.ClientSize = new System.Drawing.Size(1210, 585);
             this.Controls.Add(this.lbExams);
             this.Controls.Add(this.btnTest);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnOk);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "ChooseExamDetailsDialog";
-            this.Text = "ChooseExamDetailsDialog";
+            this.Text = "Afnamemomenten Per Vragenlijst";
             this.Load += new System.EventHandler(this.ChooseExamDialog_Load);
             this.ResumeLayout(false);
 
