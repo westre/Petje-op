9580268a8431bb2b85ff002d38648579aca9e4f5
gerritvds <gerritvds@me.com>
2015-12-09 10:44:40 +0100

    TreeView aangepast in QuestionsView, zodat er geen antwoorden meer geselecteerd kunnen worden

--- PetjeOp/PetjeOp/AddQuestionnaire/QuestionsView.Designer.cs
+++ PetjeOp/PetjeOp/AddQuestionnaire/QuestionsView.Designer.cs
@@ -41,29 +41,31 @@
             // 
             this.lblNoQuestionsInQuestionaire.AutoSize = true;
             this.lblNoQuestionsInQuestionaire.ForeColor = System.Drawing.Color.Red;
-            this.lblNoQuestionsInQuestionaire.Location = new System.Drawing.Point(254, 222);
-            this.lblNoQuestionsInQuestionaire.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblNoQuestionsInQuestionaire.Location = new System.Drawing.Point(508, 427);
+            this.lblNoQuestionsInQuestionaire.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblNoQuestionsInQuestionaire.Name = "lblNoQuestionsInQuestionaire";
-            this.lblNoQuestionsInQuestionaire.Size = new System.Drawing.Size(0, 13);
+            this.lblNoQuestionsInQuestionaire.Size = new System.Drawing.Size(0, 25);
             this.lblNoQuestionsInQuestionaire.TabIndex = 26;
             // 
             // lblNoNodeSelectedError
             // 
             this.lblNoNodeSelectedError.AutoSize = true;
             this.lblNoNodeSelectedError.ForeColor = System.Drawing.Color.Red;
-            this.lblNoNodeSelectedError.Location = new System.Drawing.Point(417, 13);
-            this.lblNoNodeSelectedError.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblNoNodeSelectedError.Location = new System.Drawing.Point(1053, 8);
+            this.lblNoNodeSelectedError.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblNoNodeSelectedError.Name = "lblNoNodeSelectedError";
-            this.lblNoNodeSelectedError.Size = new System.Drawing.Size(0, 13);
+            this.lblNoNodeSelectedError.Size = new System.Drawing.Size(211, 25);
             this.lblNoNodeSelectedError.TabIndex = 25;
+            this.lblNoNodeSelectedError.Text = "Selecteer een vraag!";
+            this.lblNoNodeSelectedError.Visible = false;
             // 
             // btnDeleteQuestion
             // 
             this.btnDeleteQuestion.Enabled = false;
-            this.btnDeleteQuestion.Location = new System.Drawing.Point(638, 61);
-            this.btnDeleteQuestion.Margin = new System.Windows.Forms.Padding(2);
+            this.btnDeleteQuestion.Location = new System.Drawing.Point(1276, 117);
+            this.btnDeleteQuestion.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnDeleteQuestion.Name = "btnDeleteQuestion";
-            this.btnDeleteQuestion.Size = new System.Drawing.Size(76, 25);
+            this.btnDeleteQuestion.Size = new System.Drawing.Size(152, 48);
             this.btnDeleteQuestion.TabIndex = 24;
             this.btnDeleteQuestion.Text = "Verwijder";
             this.btnDeleteQuestion.UseVisualStyleBackColor = true;
@@ -72,10 +74,10 @@
             // btnEditQuestion
             // 
             this.btnEditQuestion.Enabled = false;
-            this.btnEditQuestion.Location = new System.Drawing.Point(638, 24);
-            this.btnEditQuestion.Margin = new System.Windows.Forms.Padding(2);
+            this.btnEditQuestion.Location = new System.Drawing.Point(1276, 46);
+            this.btnEditQuestion.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnEditQuestion.Name = "btnEditQuestion";
-            this.btnEditQuestion.Size = new System.Drawing.Size(76, 25);
+            this.btnEditQuestion.Size = new System.Drawing.Size(152, 48);
             this.btnEditQuestion.TabIndex = 23;
             this.btnEditQuestion.Text = "Wijzig";
             this.btnEditQuestion.UseVisualStyleBackColor = true;
@@ -84,19 +86,20 @@
             // tvQuestions
             // 
             this.tvQuestions.HideSelection = false;
-            this.tvQuestions.Location = new System.Drawing.Point(2, 24);
-            this.tvQuestions.Margin = new System.Windows.Forms.Padding(2);
+            this.tvQuestions.Location = new System.Drawing.Point(4, 46);
+            this.tvQuestions.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tvQuestions.Name = "tvQuestions";
-            this.tvQuestions.Size = new System.Drawing.Size(632, 190);
+            this.tvQuestions.Size = new System.Drawing.Size(1260, 362);
             this.tvQuestions.TabIndex = 22;
+            this.tvQuestions.BeforeSelect += new System.Windows.Forms.TreeViewCancelEventHandler(this.tvQuestions_BeforeSelect);
             this.tvQuestions.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.tvQuestions_AfterSelect);
             // 
             // btnAddQuestion
             // 
-            this.btnAddQuestion.Location = new System.Drawing.Point(2, 215);
-            this.btnAddQuestion.Margin = new System.Windows.Forms.Padding(2);
+            this.btnAddQuestion.Location = new System.Drawing.Point(4, 413);
+            this.btnAddQuestion.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnAddQuestion.Name = "btnAddQuestion";
-            this.btnAddQuestion.Size = new System.Drawing.Size(240, 26);
+            this.btnAddQuestion.Size = new System.Drawing.Size(480, 50);
             this.btnAddQuestion.TabIndex = 21;
             this.btnAddQuestion.Text = "+ Voeg vraag toe";
             this.btnAddQuestion.UseVisualStyleBackColor = true;
@@ -106,16 +109,16 @@
             // 
             this.lblQuestions.AutoSize = true;
             this.lblQuestions.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblQuestions.Location = new System.Drawing.Point(-1, 4);
-            this.lblQuestions.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblQuestions.Location = new System.Drawing.Point(-2, 8);
+            this.lblQuestions.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblQuestions.Name = "lblQuestions";
-            this.lblQuestions.Size = new System.Drawing.Size(47, 13);
+            this.lblQuestions.Size = new System.Drawing.Size(87, 25);
             this.lblQuestions.TabIndex = 20;
             this.lblQuestions.Text = "Vragen";
             // 
             // QuestionsView
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.lblNoQuestionsInQuestionaire);
             this.Controls.Add(this.lblNoNodeSelectedError);
@@ -124,9 +127,9 @@
             this.Controls.Add(this.tvQuestions);
             this.Controls.Add(this.btnAddQuestion);
             this.Controls.Add(this.lblQuestions);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "QuestionsView";
-            this.Size = new System.Drawing.Size(718, 245);
+            this.Size = new System.Drawing.Size(1436, 471);
             this.Load += new System.EventHandler(this.QuestionsView_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
