8de3d37395ce805f514a250b563aae7c96e2708b
gerritvds <gerritvds@me.com>
2015-12-09 14:36:21 +0100

    Gearchiveerde vragenlijsten kunnen nu getoond worden

--- PetjeOp/PetjeOp/Questionnaires/QuestionnaireOverviewController.cs
+++ PetjeOp/PetjeOp/Questionnaires/QuestionnaireOverviewController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Design;
 using System.Windows.Forms;
 using PetjeOp.AddQuestionnaire;
 
@@ -37,7 +38,15 @@ namespace PetjeOp.Questionnaires
             Application.DoEvents();
 
             //Vraag Questionnaires op
-            Model.AllQuestionnaires = MasterController.DB.GetAllQuestionnaires();
+            if (View.cbShowArchive.Checked)
+            {
+                Model.AllQuestionnaires = MasterController.DB.GetAllQuestionnaires();
+            }
+            else
+            {
+                Model.AllQuestionnaires = MasterController.DB.GetAllActiveQuestionnaires();
+            }
+
             ResetList();
 
             //Vraag Subjects op
@@ -60,6 +69,9 @@ namespace PetjeOp.Questionnaires
                 TreeNode questionnaireTreeNode = View.tvQuestionnaires.Nodes.Add(questionnaire.Name + " (" + questionnaire.Subject + ")");
                 questionnaireTreeNode.Tag = questionnaire;
 
+                if (questionnaire.Archived)
+                    questionnaireTreeNode.ForeColor = Color.DarkGray;
+
                 //Loop over Questions
                 foreach (MultipleChoiceQuestion question in questionnaire.Questions)
                 {
@@ -67,6 +79,9 @@ namespace PetjeOp.Questionnaires
                     TreeNode questionTreeNode =
                         questionnaireTreeNode.Nodes.Add(question.QuestionIndex + ": " + question.Description);
                     questionTreeNode.Tag = question;
+
+                    if (questionnaire.Archived)
+                        questionTreeNode.ForeColor = Color.DarkGray;
 
                     //Loop over Answers
                     foreach (Answer answer in question.AnswerOptions)
@@ -168,12 +183,29 @@ namespace PetjeOp.Questionnaires
             //Als er een Node geselecteerd is, mag de knop 'Details' aangezet worden
             if (View.tvQuestionnaires.SelectedNode != null)
             {
+                Questionnaire selectedQuestionnaire = (Questionnaire) View.tvQuestionnaires.SelectedNode.Tag;
+
+                if (!selectedQuestionnaire.Archived)
+                {
+                    View.btnDelete.Enabled = true;
+                }
+                else
+                {
+                    View.btnDelete.Enabled = false;
+                }
+
                 View.btnDetails.Enabled = true;
             }
             else
             {
                 View.btnDetails.Enabled = false;
+                View.btnDelete.Enabled = false;
             }
+        }
+
+        public void DeleteQuestionnaire()
+        {
+            
         }
     }
 }
