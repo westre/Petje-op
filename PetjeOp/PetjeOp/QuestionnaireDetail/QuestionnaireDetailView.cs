acf94847cc8f14041926a1fa0e72fe69857c9026
ThijsvanDam <thijsvandam@msn.com>
2015-12-09 11:14:07 +0100

    Comments QuestionnaireDetail

--- PetjeOp/PetjeOp/QuestionnaireDetail/QuestionnaireDetailView.cs
+++ PetjeOp/PetjeOp/QuestionnaireDetail/QuestionnaireDetailView.cs
@@ -20,17 +20,20 @@ namespace PetjeOp.QuestionnaireDetail
             Controller = controller;
         }
 
+        //Ga terug naar het vragenlijstoverzicht wanneer je op de knop drukt.
         private void btnCancel_Click(object sender, EventArgs e)
         {
             Controller.GoToQuestionnaireOverview();
         }
 
+        //Wanneer het scherm geladen wordt, set dan de labels en vul de combobox.
         private void QuestionnaireDetailView_Load(object sender, EventArgs e)
         {
             Controller.setLabels();
             Controller.fillCbSelectQuestionnaire();
         }
 
+        //Wanneer de index van de combobox wordt gewijzigd, verander de huidige questionnaire, set de labels opnieuw en update de treeview.
         private void cbSelectQuestionnaire_SelectedIndexChanged(object sender, EventArgs e)
         {
             Controller.Model.Questionnaire = (Questionnaire)cbSelectQuestionnaire.SelectedItem;
@@ -38,10 +41,9 @@ namespace PetjeOp.QuestionnaireDetail
             Controller.View.questionsView1.UpdateTreeView();
         }
 
+        //Wanneer de afnamemomentknop aangeklikt wordt, open het examendialoog.
         private void btnExams_Click(object sender, EventArgs e)
         {
-            
-
             ChooseExamDetailsDialog ExamsDialog = new ChooseExamDetailsDialog(Controller);
             ExamsDialog.ShowDialog();
         }
